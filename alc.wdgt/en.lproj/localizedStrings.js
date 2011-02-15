d720576e343fa97f1778c9918af2a1e9a06b21e9
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2011-02-14 23:04:48 -0800

    updated .wdgt

--- alc.wdgt/en.lproj/localizedStrings.js
+++ alc.wdgt/en.lproj/localizedStrings.js
@@ -5,22 +5,6 @@
  code generator otherwise the changes will be lost.
  */
 var localizedStrings = {
-    "My RSS Feed": "My RSS Feed",
-    "New URL: ": "New URL: ",
-    "Set": "Set",
-    "Feed URLs: ": "Feed URLs: ",
-    "Article Length": "Article Length",
-    "Reset": "Reset",
-    "Done": "Done",
-    "Invalid Feed": "Invalid Feed",
-    "%s does not appear to be a valid RSS or Atom feed.": "%s does not appear to be a valid RSS or Atom feed.",
-    "No Items Found": "No Items Found",
-    "The feed does not contain any entries.": "The feed does not contain any entries.",
-    "No items matched the search terms.": "No items matched the search terms.",
-    "Nothing To Display": "Nothing To Display",
-    "The feed does not contain any entries within the specified criteria.": "The feed does not contain any entries within the specified criteria.",
-    "Loading": "Loading",
-    "%s new": "%s new",
-    "No Feed Specified": "No Feed Specified",
-    "Please specify a valid feed URL.": "Please specify a valid feed URL."
+    "Hello, World!": "Hello, World!",
+    "Done": "Done"
 };
